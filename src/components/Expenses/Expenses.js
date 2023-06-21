6b938060766e6e46b48d638905386790276763cd
puneet <puneet25j@gmail.com>
2023-06-21 22:48:23 +0530

    initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import "./Expenses.css";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [filteredYear, setfilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setfilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChange={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (expenseData) => {
+    const expense = {
+      ...expenseData,
+      id: Math.random(),
+    };
+    props.onAddExpense(expense);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add Expenses</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+}
+
+export default NewExpense;
